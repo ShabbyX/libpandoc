22f550397ff665a1feb2bc39949f2ce3fdedb839
Shahbaz Youssefi <ShabbyX@gmail.com>
2015-03-20 16:38:07 +0100

    Improve C header file
    This allows the C header file to be included in C++ applications as
    well.
    
    Furthermore, the strings that are not supposed to be modified are made
    const.  This is for format and settings specifiers (which could be
    string literals), as well as the writer callback which should just read
    from the buffer.
    
    Signed-off-by: Shahbaz Youssefi <ShabbyX@gmail.com>

--- src/pandoc.h
+++ src/pandoc.h
@@ -8,16 +8,21 @@
 
 #ifndef _PANDOC_H
 #define _PANDOC_H 1
+
 #include <wchar.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /*
  * Initializes the Haskell runtime. Every call to this function should
  * be matched with exactly one call to `pandoc_exit`.
  */
-extern void pandoc_init();
+void pandoc_init();
 
 /* Shuts down the Haskell runtime.  */
-extern void pandoc_exit();
+void pandoc_exit();
 
 /*
  * Calls `pandoc` with given input and output formats and streams.
@@ -26,9 +31,13 @@ extern void pandoc_exit();
  * distributed with `libpandoc`.  Settings can be `NULL`.  All strings
  * should be encoded as UTF-8.  User data is any pointer.
  */
-extern char *pandoc(int buffer_size,
-                    char *input_format, char *output_format, char *settings,
-                    int (*reader)(char *, void *), void (*writer)(char *, int, void *),
-                    void *user_data);
+char *pandoc(int buffer_size,
+            const char *input_format, const char *output_format, const char *settings,
+            int (*reader)(char *, void *), void (*writer)(const char *, int, void *),
+            void *user_data);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* !_PANDOC_H */
